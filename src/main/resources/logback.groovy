12153a0202edc38fe4d1e563bdd9cb76fe55bb99
Pete Colapietro <pcssi@users.noreply.github.com>
2015-10-15 14:18:15 -0400

    Adding logback configuration.

---
+++ src/main/resources/logback.groovy
@@ -0,0 +1,17 @@
+import ch.qos.logback.classic.encoder.PatternLayoutEncoder
+import ch.qos.logback.core.ConsoleAppender
+
+import static ch.qos.logback.classic.Level.WARN
+
+final patternString = "%d{HH:mm:ss.SSS} [%thread] %-5level %logger{5} Groovy - %msg%n"
+
+
+final STDOUT = "STDOUT"
+
+appender(STDOUT, ConsoleAppender) {
+  encoder(PatternLayoutEncoder) {
+    pattern = patternString
+  }
+}
+
+root(WARN, [STDOUT])
